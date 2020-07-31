289a30e0fd4ad6d5b909d9ba4e5df580d67bb810
emil-johansen <61498642+emil-johansen@users.noreply.github.com>
2020-07-31 22:30:48 +0200

    Add files via upload

---
+++ Speaker-Boost-button.sh
@@ -0,0 +1,10 @@
+# You can change the volume by just changing 70 to some thing else like "fpp -v 35" puts the volume at 35%
+# Examples on time config :
+# sleep 5s = Wait 5s
+# sleep 5m = Whait 5m
+# sleep 5h = Whait 5h
+# The script:
+fpp -v 70
+sleep 5
+fpp -v 30
+
