d0e472aa4f490018f4ad684a57a4fc403f822ef3
emil-johansen <61498642+emil-johansen@users.noreply.github.com>
2020-08-01 00:03:44 +0200

    Update Speaker-Boost-button.sh
    Now the music fades in and out

--- Speaker-Boost-button.sh
+++ Speaker-Boost-button.sh
@@ -1,17 +1,21 @@
+# This script can be used if you have a speaker in your show and dont want to play loud music all the time. you can have a button the audience can press and trigger this script
 # You can change the volume by just changing 70 to some thing else like "fpp -v 35" puts the volume at 35%
 # Examples on time config :
-# sleep 5s = Wait 5s
-# sleep 5m = Whait 5m
-# sleep 5h = Whait 5h
-# The script:
-#############################
+
+#####################################################
+# ONLY CHANGE THE NUMBERS AND BETWEEN THE ###
 #volume at beginning and end:
 e=30
 #volume at peak:
 v=70
+#Time the audio is at peak
+# 5s = Wait 5 seconds
+# 5m = Whait 5 minutes
+# 5h = Whait 5 hours
+t=3m
 # Speed: (not defined in sec)(do not set to low else it will begin to lag)
 s=5
-#############################
+#######################################################
 
 i=$e
 until [ $i -gt $((v-s)) ]
@@ -20,7 +24,7 @@ do
   ((i=i+$s))
 fpp -v $i
 done
-sleep 5
+sleep $t
 while [ $i -ge $((e+s)) ]
 do
   echo Number: $i
